795716a720f6a25a03061188959eaca14a697556
SadeeshaPerera <121984253+SadeeshaPerera@users.noreply.github.com>
2024-09-07 17:38:33 +0530

    feat: Implemented basic auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'auth_gate.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
   runApp(const MyApp());
 }
 
@@ -14,10 +23,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const AuthGate(),
     );
   }
 }
+
+
+
+
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});

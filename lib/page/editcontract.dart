ad771afcc311dc34df6f69d63b82aff3ee864ef9
SadeeshaPerera <121984253+SadeeshaPerera@users.noreply.github.com>
2024-09-10 16:57:22 +0530

    chore: Updated

---
+++ lib/page/editcontract.dart
@@ -0,0 +1,179 @@
+import '/page/contractlistpage.dart';
+import 'package:flutter/material.dart';
+
+import '../models/contract.dart';
+import '../services/contract_firebase_crud.dart';
+
+class EditContract extends StatefulWidget {
+  final Contract? contract;
+  EditContract({this.contract});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _EditPage();
+  }
+}
+
+class _EditPage extends State<EditContract> {
+  final _contract_name = TextEditingController();
+  final _contract_position = TextEditingController();
+  final _contract_contact = TextEditingController();
+  final _docid = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _docid.value = TextEditingValue(text: widget.contract!.uid.toString());
+    _contract_name.value =
+        TextEditingValue(text: widget.contract!.contractname.toString());
+    _contract_position.value =
+        TextEditingValue(text: widget.contract!.position.toString());
+    _contract_contact.value =
+        TextEditingValue(text: widget.contract!.contactno.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final DocIDField = TextField(
+        controller: _docid,
+        readOnly: true,
+        autofocus: false,
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Name",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))));
+
+    final nameField = TextFormField(
+        controller: _contract_name,
+        autofocus: false,
+        validator: (value) {
+          if (value == null || value.trim().isEmpty) {
+            return 'This field is required';
+          }
+        },
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Name",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))));
+    final positionField = TextFormField(
+        controller: _contract_position,
+        autofocus: false,
+        validator: (value) {
+          if (value == null || value.trim().isEmpty) {
+            return 'This field is required';
+          }
+        },
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Position",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))));
+    final contactField = TextFormField(
+        controller: _contract_contact,
+        autofocus: false,
+        validator: (value) {
+          if (value == null || value.trim().isEmpty) {
+            return 'This field is required';
+          }
+        },
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Contact Number",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))));
+
+    final viewListbutton = TextButton(
+        onPressed: () {
+          Navigator.pushAndRemoveUntil<dynamic>(
+            context,
+            MaterialPageRoute<dynamic>(
+              builder: (BuildContext context) => ContractListPage(),
+            ),
+            (route) => false, //if you want to disable back feature set to false
+          );
+        },
+        child: const Text('View List of Contract'));
+
+    final SaveButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Theme.of(context).primaryColor,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () async {
+          if (_formKey.currentState!.validate()) {
+            var contractresponse = await FirebaseCrud.updatecontract(
+                name: _contract_name.text,
+                position: _contract_position.text,
+                contactno: _contract_contact.text,
+                docId: _docid.text);
+            if (contractresponse.code != 200) {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text(contractresponse.message.toString()),
+                    );
+                  });
+            } else {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text(contractresponse.message.toString()),
+                    );
+                  });
+            }
+          }
+        },
+        child: Text(
+          "Update",
+          style: TextStyle(color: Theme.of(context).primaryColorLight),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('FreeCode Spot'),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  DocIDField,
+                  const SizedBox(height: 25.0),
+                  nameField,
+                  const SizedBox(height: 25.0),
+                  positionField,
+                  const SizedBox(height: 35.0),
+                  contactField,
+                  viewListbutton,
+                  const SizedBox(height: 45.0),
+                  SaveButon,
+                  const SizedBox(height: 15.0),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

28376abcf57e7ef96eaa5ae9905b6785a11ee942
SadeeshaPerera <121984253+SadeeshaPerera@users.noreply.github.com>
2024-10-01 05:20:58 +0530

    feat Implement basic dashboard ui

--- lib/page/admin/admin_main_screen.dart
+++ lib/page/admin/admin_main_screen.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shopping_mall_application/page/additem.dart';
 import 'package:shopping_mall_application/page/addpromotion.dart';
-
 import 'package:shopping_mall_application/page/admin/admin_incident_list_page.dart';
-
-import 'package:shopping_mall_application/page/check_rental_applications.dart'; // Import the CheckRentalApplications page
-import 'package:shopping_mall_application/page/maintenance_request_list_page.dart'; // Import the MaintenanceRequestList page
-import 'package:shopping_mall_application/auth_gate.dart'; // Import your authentication gate
+import 'package:shopping_mall_application/page/check_rental_applications.dart';
+import 'package:shopping_mall_application/page/maintenance_request_list_page.dart';
+import 'package:shopping_mall_application/auth_gate.dart';
 
 class AdminScreen extends StatelessWidget {
   const AdminScreen({super.key});
@@ -16,98 +14,138 @@ class AdminScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Admin Page'),
+        title: const Text('Admin Dashboard'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Welcome to the Admin Page!',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(
-                height:
-                    20), // Add some spacing between the text and the buttons
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 600) {
+            // Web layout
+            return buildWebLayout(context);
+          } else {
+            // Mobile layout
+            return buildMobileLayout(context);
+          }
+        },
+      ),
+    );
+  }
 
-            // Button to add an inventory item
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          AddItem()), // Navigate to AddItem page
-                );
-              },
-              child: const Text('Add an Inventory Item'),
-            ),
+  Widget buildWebLayout(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: GridView.count(
+        crossAxisCount: 3,
+        crossAxisSpacing: 16.0,
+        mainAxisSpacing: 16.0,
+        children: buildDashboardItems(context),
+      ),
+    );
+  }
 
-            // Button to create a promotion
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          AddPromotion()), // Navigate to AddPromotion page
-                );
-              },
-              child: const Text('Create a Promotion'),
-            ),
+  Widget buildMobileLayout(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: buildDashboardItems(context),
+      ),
+    );
+  }
 
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => AdminIncidentListPage()));
-              },
-              child: const Text('View Reported Incidents'),
-            ),
-            // Button to check rental applications
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          CheckRentalApplications()), // Navigate to CheckRentalApplications page
-                );
-              },
-              child: const Text('Check Rental Applications'),
-            ),
+  List<Widget> buildDashboardItems(BuildContext context) {
+    return [
+      buildDashboardItem(
+        context,
+        icon: Icons.inventory,
+        label: 'Add an Inventory Item',
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddItem()),
+          );
+        },
+      ),
+      buildDashboardItem(
+        context,
+        icon: Icons.local_offer,
+        label: 'Create a Promotion',
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddPromotion()),
+          );
+        },
+      ),
+      buildDashboardItem(
+        context,
+        icon: Icons.report,
+        label: 'View Reported Incidents',
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AdminIncidentListPage()),
+          );
+        },
+      ),
+      buildDashboardItem(
+        context,
+        icon: Icons.assignment,
+        label: 'Check Rental Applications',
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CheckRentalApplications()),
+          );
+        },
+      ),
+      buildDashboardItem(
+        context,
+        icon: Icons.build,
+        label: 'View Maintenance Requests',
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => MaintenanceRequestListPage()),
+          );
+        },
+      ),
+      buildDashboardItem(
+        context,
+        icon: Icons.logout,
+        label: 'Sign Out',
+        onPressed: () async {
+          await FirebaseAuth.instance.signOut();
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const AuthGate()),
+          );
+        },
+      ),
+    ];
+  }
 
-            // Button to view maintenance requests
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          MaintenanceRequestListPage()), // Navigate to MaintenanceRequestListPage
-                );
-              },
-              child: const Text('View Maintenance Requests'),
+  Widget buildDashboardItem(BuildContext context,
+      {required IconData icon,
+      required String label,
+      required VoidCallback onPressed}) {
+    return Card(
+      elevation: 4.0,
+      child: InkWell(
+        onTap: onPressed,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(icon, size: 48.0, color: Theme.of(context).primaryColor),
+                const SizedBox(height: 16.0),
+                Text(label,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 16.0)),
+              ],
             ),
-
-            const SizedBox(
-                height: 20), // Add some spacing before the sign-out button
-
-            // Sign-Out Button
-            ElevatedButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut(); // Sign out from Firebase
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          const AuthGate()), // Navigate back to the sign-in page
-                );
-              },
-              child: const Text('Sign Out'),
-            ),
-          ],
+          ),
         ),
       ),
     );
